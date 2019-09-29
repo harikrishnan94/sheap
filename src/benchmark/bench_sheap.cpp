72088cbf75448b7548005370c9400718a29a7aa2
Harikrishnan Prabhakharan <harikrishnan.prabakaran@gmail.com>
2019-09-29 11:41:48 +0530

    Add benchmark for sheap vs malloc

--- src/benchmark/bench_sheap.cpp
+++ src/benchmark/bench_sheap.cpp
@@ -1,1 +1,99 @@
+#include "sheap/Sheap.h"
+
+#include <algorithm>
 #include <benchmark/benchmark.h>
+#include <cstdlib>
+#include <iterator>
+#include <random>
+#include <vector>
+
+class MallocAllocator {
+public:
+  void *alloc(std::size_t size) { return std::malloc(size); }
+  void free(void *ptr) { std::free(ptr); }
+};
+
+class SheapAllocator {
+public:
+  SheapAllocator()
+      : mem(std::malloc(MAX_MEMORY)), sheap(mem, MAX_MEMORY, config) {}
+
+  ~SheapAllocator() { std::free(mem); }
+
+  void *alloc(std::size_t size) { return sheap.alloc(0, size); }
+  void free(void *ptr) { sheap.free(ptr); }
+
+private:
+  static constexpr auto MAX_MEMORY = 40'000'000;
+  static constexpr auto config = sheap::config{1, 64 * 1024, 1};
+
+  void *mem;
+  sheap::Sheap sheap;
+};
+
+template <typename Allocator>
+static void BM_AllocFree(benchmark::State &s, Allocator &&a) {
+  constexpr auto BATCH_SIZE = 100'000;
+  constexpr auto MIN_ALLOCSIZE = 32;
+  constexpr auto MAX_ALLOCSIZE = 4096;
+
+  std::mt19937 gen{std::random_device{}()};
+  std::uniform_int_distribution<std::int64_t> dist{MIN_ALLOCSIZE,
+                                                   MAX_ALLOCSIZE};
+  std::vector<std::int64_t> sizes;
+  std::vector<void *> to_free;
+  std::size_t block_size = s.range();
+
+  auto prep_batch = [&]() {
+    s.PauseTiming();
+    sizes.clear();
+    std::generate_n(std::back_inserter(sizes), BATCH_SIZE,
+                    [&gen, &dist]() { return dist(gen); });
+    s.ResumeTiming();
+  };
+  auto free_all = [&]() {
+    for (auto p : to_free) {
+      a.free(p);
+    }
+
+    to_free.clear();
+  };
+
+  while (s.KeepRunningBatch(BATCH_SIZE)) {
+    prep_batch();
+    for (auto i = 0; i < BATCH_SIZE; i++) {
+      auto ptr = a.alloc(sizes[i]);
+
+      if (ptr == nullptr) {
+        s.SkipWithError("OOM");
+        break;
+      }
+
+      to_free.push_back(ptr);
+
+      if (to_free.size() == block_size)
+        free_all();
+    }
+  }
+
+  free_all();
+}
+
+BENCHMARK_CAPTURE(BM_AllocFree, SheapAlloc, SheapAllocator{})
+    ->ThreadRange(1, std::thread::hardware_concurrency())
+    ->UseRealTime()
+    ->Arg(100)
+    ->Arg(500)
+    ->Arg(1000)
+    ->Arg(2000)
+    ->Arg(5000)
+    ->Arg(10000);
+BENCHMARK_CAPTURE(BM_AllocFree, Malloc, MallocAllocator{})
+    ->ThreadRange(1, std::thread::hardware_concurrency())
+    ->UseRealTime()
+    ->Arg(100)
+    ->Arg(500)
+    ->Arg(1000)
+    ->Arg(2000)
+    ->Arg(5000)
+    ->Arg(10000);
